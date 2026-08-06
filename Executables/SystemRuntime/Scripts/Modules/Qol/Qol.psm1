3858b70deae677e786136b01a0856e9ef06ee430
Özkan Çırak <136706007+ozkancirak@users.noreply.github.com>
2026-08-06 13:14:13 +0300

    fix: correct feedback policy registry path

--- Executables/SystemRuntime/Scripts/Modules/Qol/Qol.psm1
+++ Executables/SystemRuntime/Scripts/Modules/Qol/Qol.psm1
@@ -392,7 +392,7 @@ function Disable-TouchVisualFeedback {
 function Disable-WindowsFeedback {
     reg add "HKCU\SOFTWARE\Microsoft\Siuf\Rules" /v "NumberOfSIUFInPeriod" /t REG_DWORD /d 0 /f
     reg delete 'HKCU\SOFTWARE\Microsoft\Siuf\Rules' /v 'PeriodInNanoSeconds' /f
-    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v "DoNotShowFeedbackNotifications" /t REG_DWORD /d 1 /f
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "DoNotShowFeedbackNotifications" /t REG_DWORD /d 1 /f
 }
 
 # Function to disable Windows Spotlight
